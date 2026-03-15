90e89fe4e17400397191fc97094c20dcf6fca2c8
puddi <pudding050@gmail.com>
2026-03-15 14:15:35 +0800

    程式找不到 models修正

--- public/Room.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const roomSchema = new mongoose.Schema({
-    name: { type: String, unique: true },
-    createdAt: { type: Number, default: Date.now }
-});
-
-module.exports = mongoose.model('Room', roomSchema);
